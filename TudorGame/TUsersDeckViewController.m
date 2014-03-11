a55763955bec3f197dfd9089fc78754cf6382c26
David Joerg <deejay@dhcp176.santec.healthnet.lu>
2014-03-11 16:27:08 +0100

    -added drag n drop function (Not ready yet)

--- TudorGame/TUsersDeckViewController.m
+++ TudorGame/TUsersDeckViewController.m
@@ -16,11 +16,14 @@
 #import "DataManager.h"
 #import "AppSpecificValues.h"
 
+
 @interface TUsersDeckViewController ()
 @property(nonatomic, retain) Player *player;
 @property(nonatomic, retain) DataManager *dataManager;
-@property(nonatomic, retain) NSArray *dataArrayAllCards;
-@property(nonatomic, retain) NSArray *dataArrayCurrentCardDeck;
+@property(nonatomic, retain) NSMutableArray *dataArrayAllCards;
+@property(nonatomic, retain) NSMutableArray *dataArrayCurrentCardDeck;
+@property(nonatomic, retain) UIImageView *cardDublicate;
+@property(nonatomic, retain)  Product *productTouched;
 
 @end
 
@@ -41,7 +44,7 @@
     
     self.dataManager = [DataManager sharedManager];
     self.player = self.dataManager.player;
-    
+    self.dataArrayCurrentCardDeck = [[NSMutableArray alloc]init];
     if([self.player.status isEqualToString:USER_LOGGED_IN])
     {
         [self loadProducts];
@@ -74,8 +77,9 @@
     
     [SpinnerView loadSpinnerIntoViewController:self withText:@"loading cards..." andBtnTouched:@selector(backBtnTouched:)];
         
-        
-        dispatch_queue_t myNewQueue = dispatch_queue_create("loadingCards", NULL);
+    
+    
+        /*dispatch_queue_t myNewQueue = dispatch_queue_create("loadingCards", NULL);
         
         // Dispatch work to your queue
         dispatch_async(myNewQueue, ^
@@ -92,9 +96,20 @@
                            });
                            
                            
-                       });
-        
-        
+                       });*/
+    
+    
+    self.dataArrayAllCards = [NSMutableArray arrayWithArray: self.player.products];
+    self.dataArrayCurrentCardDeck = [NSMutableArray arrayWithArray:self.player.cardsInDeck];
+    //self.dataArrayCurrentCardDeck = self.user.productsChoosenForDeck;
+    
+    //self.dataArrayCurrentCardDeck = self.player.products;
+    
+    [self.collectionView reloadData];
+    [self.collectionViewCurrentDeck reloadData];
+    
+    [SpinnerView removeSpinnerFromViewController:self];
+    
     
 }
 
@@ -203,14 +218,111 @@
 
 -(void)productsUpdated:(NSDictionary *)updatedProducts
 {
-    self.dataArrayAllCards = self.player.products;
+    self.dataArrayAllCards = [NSMutableArray arrayWithArray: self.player.products];
     //self.dataArrayCurrentCardDeck = self.user.productsChoosenForDeck;
     
-    self.dataArrayCurrentCardDeck = self.player.products;
+    //self.dataArrayCurrentCardDeck = self.player.products;
     
     [self.collectionView reloadData];
     [self.collectionViewCurrentDeck reloadData];
     
     [SpinnerView removeSpinnerFromViewController:self];
 }
+
+- (IBAction)handlePan:(UIPanGestureRecognizer *)recognizer
+{
+    //CGPoint touch = [recognizer locationInView:self.collectionView];
+
+     CGPoint point = [recognizer locationInView:self.collectionView];
+    //CGRect frame =  [self.cardDetailView convertRect:self.cardDetailView.bounds toView:self.view];
+    NSIndexPath *path =  [self.collectionView indexPathForItemAtPoint:point];
+    if(path)
+    {
+        //Touch starts
+        if(recognizer.state == UIGestureRecognizerStateBegan)
+        {
+         
+            
+            TCardCollectionViewCell *cardField =(TCardCollectionViewCell*) [self.collectionView cellForItemAtIndexPath:path];
+
+            self.productTouched = [self.dataArrayAllCards objectAtIndex:path.row];
+            
+            
+            TOverviewCardViewController *detailVc = [CardGenerator generateOverviewCard:self.productTouched];
+            UIImage *snapshot = [detailVc snapshotView];
+
+            
+            //dubliziere aktuelle Karte und mache altes Feld frei
+            self.cardDublicate = [[UIImageView alloc] initWithFrame:cardField.frame];
+            self.cardDublicate.image = snapshot;
+            self.cardDublicate.center = [self.collectionView convertPoint:point toView:self.view];
+            [self.view addSubview:self.cardDublicate];
+            [self.view bringSubviewToFront:self.cardDublicate];
+        }
+    }
+    //Dragging
+    if(recognizer.state == UIGestureRecognizerStateChanged)
+    {
+        
+        CGPoint delta = [recognizer translationInView: self.cardDublicate.superview];
+        CGPoint c = self.cardDublicate.center;
+        c.x += delta.x; c.y += delta.y;
+        self.cardDublicate.center = c;
+        [recognizer setTranslation: CGPointZero inView: self.cardDublicate.superview];
+    }
+    //Touch ended
+    if(recognizer.state == UIGestureRecognizerStateEnded)
+    {
+        UIView *touchedView = [self.view hitTest:self.cardDublicate.center withEvent:nil];
+
+        if([touchedView isKindOfClass:[UICollectionView class]])
+        {
+            UICollectionView *collectionView = (UICollectionView*) touchedView;
+            
+            if(collectionView == self.collectionViewCurrentDeck)
+            {
+                [self.dataArrayCurrentCardDeck addObject:self.productTouched];
+                [self.collectionViewCurrentDeck reloadData];
+                
+                [self.dataArrayAllCards removeObject:self.productTouched];
+                [self.collectionView reloadData];
+                
+                [self.cardDublicate removeFromSuperview];
+                self.productTouched = nil;
+                
+               // [self.dataManager saveProducts:self.dataArrayAllCards andProductsInDeck:self.dataArrayCurrentCardDeck];
+            }
+        }
+    }
+
+}
+
+-(IBAction)handleSingleTap:(UITapGestureRecognizer*)recognizer
+{
+    
+    CGPoint touch = [recognizer locationInView:self.view];
+    UIView *touchedView = [self.view hitTest:touch withEvent:nil];
+    //CGRect frame =  [self.cardDetailView convertRect:self.cardDetailView.bounds toView:self.view];
+    
+    if([touchedView isKindOfClass:[TCardCollectionViewCell class]])
+    {
+        TCardCollectionViewCell *cardField = (TCardCollectionViewCell*) touchedView;
+        NSIndexPath *path = [self.collectionView indexPathForCell:cardField];
+        Product *productTouched = [self.dataArrayAllCards objectAtIndex:path.row];
+
+        
+        TDetailCardViewController *detailVc = [CardGenerator generateDetailCard:productTouched];
+        UIImage *snapshot = [detailVc snapshotView];
+       
+        
+        UIImageView *view = [[UIImageView alloc] initWithFrame:cardField.frame];
+        view.image = snapshot;
+        
+            NSLog(@"card touched");
+        
+    }
+    
+}
+
+
 @end
