10200cd83b919a293fa32c0e263eb43e231f7993
David Joerg <deejay@dhcp176.santec.healthnet.lu>
2014-03-11 16:29:54 +0100

    -discard changes

--- TudorGame/UserData.h
+++ TudorGame/UserData.h
@@ -2,17 +2,20 @@
 //  UserData.h
 //  TudorGame
 //
-//  Created by David Joerg on 05.02.14.
+//  Created by David Joerg on 11.03.14.
 //  Copyright (c) 2014 David Joerg. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
 
+@class ProductData;
 
 @interface UserData : NSManagedObject
 
-@property (nonatomic, retain) NSString * name;
 @property (nonatomic, retain) NSString * password;
+@property (nonatomic, retain) NSString * name;
 
 @end
+
+
--- TudorGame/UserData.m
+++ TudorGame/UserData.m
@@ -2,16 +2,18 @@
 //  UserData.m
 //  TudorGame
 //
-//  Created by David Joerg on 05.02.14.
+//  Created by David Joerg on 11.03.14.
 //  Copyright (c) 2014 David Joerg. All rights reserved.
 //
 
 #import "UserData.h"
 
 
+
 @implementation UserData
 
-@dynamic name;
 @dynamic password;
+@dynamic name;
+
 
 @end
